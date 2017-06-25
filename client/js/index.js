12482775556d27f49a5ab047e164eaf5800cf265
Nikola Phipps <tesla.phipps@gmail.com>
2017-06-25 03:17:01 -0700

    set up tabs

--- client/js/index.js
+++ client/js/index.js
@@ -1,7 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, {Component} from "react";
+import ReactDOM, {render} from "react-dom";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('root')
-);
+class App extends Component {
+  condtructor() {
+    this.state = {tabIndex: 0}
+  }
+  render() {
+    return (
+      <Tabs>
+        <TabList>
+          <Tab>Title 1</Tab>
+          <Tab>Title 2</Tab>
+        </TabList>
+        <TabPanel>
+          <p>Some stuff</p>
+        </TabPanel>
+        <TabPanel>
+          <p>some other stuff</p>
+        </TabPanel>
+      </Tabs>
+    );
+  }
+}
+render(<App/>, document.getElementById('root'));
